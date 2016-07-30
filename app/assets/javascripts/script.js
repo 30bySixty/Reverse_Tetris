7d553417f11a8ad1d23a601c5a110b8791333359
Tatyana Shestopalova <tshestopalova@gmail.com>
2016-07-30 18:37:17 -0500

    Updates pieceLogic to check L & R when colOffset == D

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,30 +1,20 @@
-var $pieces = [];
+//BEGIN SETUP OF GAME BOARD & SCORING
+var start = 0;
 var width = 10;
 var height = 15;
 var num = 15;
-var block;
-var blockClass;
-var score;
-var rows = [];
-var start = 0;
 var rowScore = 0;
-var solutions = [];
-var blocks = [];
-//var blockformulas = [
-//    [[0,0], [1,0], [2,0], [1,1]], //T shape (B)
-//    [[0,0], [1,0], [2,0], [3,0]], //| shape horizontal (A) 
-//    [[0,0], [0,1], [0,2], [0,3]], //| shape vertical (A) 
-//    [[0,0], [1,0], [0,1], [1,1]], //square shape (C)
-//    [[0,1], [1,0], [1,1], [2,0]], //S shape (D)
-//    [[0,0], [0,1], [1,0], [2,0]]  //L shape (E)
-//];
-function logscore(X) {
-    rowScore += X;
-    $(".notice3").remove();
-    if (rowScore > 0) {
-        $("#notes").append('<div class="notice3">Number of rows cleared: '+rowScore+'</div>');
-    };
-};
+var createboard = function() {
+    var i = 0;
+    var j = 0;
+    for (i=start; i<num; i++) {
+        $('.board').prepend('<div class="rowT" id="'+i+'"></div>');
+        for (j=0; j<width; j++) {
+            $('.board').children("#"+i).append('<div id="'+j+'"></div>');
+        };
+    }
+    orderrows();
+}
 function orderrows() {
     var i = 0;
     $('.board').children().each(function() {
@@ -35,64 +25,19 @@ function orderrows() {
     $('.board').children().children().css("border-top", "1px solid lightslategray");
     $('.board').children('#1').children().css("border-top", "2px solid red");
 };
-var createboard = function() {
-    var i = 0;
-    var j = 0;
-//    console.log(start);
-//    console.log(num);
-    for (i=start; i<num; i++) {
-        $('.board').prepend('<div class="rowT" id="'+i+'"></div>');
-        for (j=0; j<width; j++) {
-            $('.board').children("#"+i).append('<div id="'+j+'"></div>');
-        };
-    }
-    orderrows();
-}
-function validateForm() {
-    //REMOVE PREVIOUS NOTICES
-    $(".error").remove();
-    $(".form-group").removeClass("has-error");
-    $(".notice").remove();
-//    $(".notice2").remove();
-    //DISPLAY ERROR MESSAGES, IF ANY
-    var piece = document.getElementById("piece").value;
-    if (piece == null || piece == "") {
-        $("#pieceBlock").append('<div class="error">Piece cannot be blank.</div>');
-        $('#pieceBlock:contains(.)').closest('.form-group').addClass('has-error');
-        return false;
-    } else if (piece.match(/^(A|B|C|D|E)$/)) {
-        $("#pieceBlock").append('<div class="notice">Your piece is piece '+piece+'.</div>');
-        $(".form-horizontal")[0].reset();
-        myFunction(piece);   
-    } else {
-        $("#pieceBlock").append('<div class="error">Input must be A, B, C, D, or E.</div>');
-        $('#pieceBlock:contains(.)').closest('.form-group').addClass('has-error');
-        return false;
-    };
-}
+//END SETUP OF GAME BOARD & SCORING
+
+//BEGIN LOGIC FOR PLACING BLOCKS
+var $pieces = [];
+var blockClass;
+var blocks = [];
+var block;
+//DETERMINE PIECE LOGIC BASED ON COLUMN OFFSET OF BLOCK COMPONENTS VS INITIAL BLOCK COMPONENT POSITION
 function myFunction(piece) {
     $pieces.push(piece);
-//    $("#pieceBlock").append('<div class="notice2"><p>List of all pieces: '+$pieces.join(" ")+'</p></div>'); 
     var choose = piece.id 
     switch (choose) {
-        case "B":
-            piece = "B";
-            blockClass = "B";
-            blocks = [[[0,0], [1,0], [1,1], [2,0]], [[0,0], [0,1], [0,2], [1,1], [1,0]], [[0,0], [0,1], [-1,1], [1,1], [-1,0], [1,0]], [[0,0], [0,1], [0,2], [-1,1], [-1,0]]];
-            for (i=0; i<blocks.length; i++) {
-                block = blocks[i];
-                if (i==0) {
-                    pieceLogic(2, 0, 1, 1, 0);
-                } else if (i==1) {
-                    pieceLogic(0, 0, 1, 0, 1, 1, 2);
-                } else if (i==2) {
-                    pieceLogic(0, 0, 1, 1, 2);
-                } else {
-                    pieceLogic(0, 0, 0, -1, 3, 2, 2);
-                };
-            };
-            chooseSolution();
-            break;
+        //Shape "|"
         case "A":
             piece = "A";
             blockClass = "A";
@@ -107,6 +52,26 @@ function myFunction(piece) {
             };
             chooseSolution();
             break;
+        //Shape "T"
+        case "B":
+            piece = "B";
+            blockClass = "B";
+            blocks = [[[0,0], [1,0], [1,1], [2,0]], [[0,0], [0,1], [0,2], [1,1], [1,0]], [[0,0], [0,1], [-1,1], [1,1], [-1,0], [1,0]], [[0,0], [0,1], [0,2], [-1,1], [-1,0]]];
+            for (i=0; i<blocks.length; i++) {
+                block = blocks[i];
+                if (i==0) {
+                    pieceLogic(2, 0, 1, 1, 0);
+                } else if (i==1) {
+                    pieceLogic(0, 0, 1, 0, 1, 1, 2);
+                } else if (i==2) {
+                    pieceLogic(0, 0, 1, -1, 2);//CHANGED to -1 FROM 1
+                } else {
+                    pieceLogic(0, 0, 0, -1, 3, 2, 2);
+                };
+            };
+            chooseSolution();
+            break;
+        //Shape "Square"
         case "C":
             piece = "C";
             blockClass = "C";
@@ -115,6 +80,7 @@ function myFunction(piece) {
             pieceLogic(1, 0, 1, 0, 0);
             chooseSolution();
             break;
+        //Shape "S"
         case "D":
             piece = "D";
             blockClass = "D";
@@ -129,6 +95,7 @@ function myFunction(piece) {
             };
             chooseSolution();
             break;
+        //Shape "L"
         case "E":
             piece = "E";
             blockClass = "E";
@@ -140,7 +107,7 @@ function myFunction(piece) {
                 } else if (i==1) {
                     pieceLogic(0, 0, 1, 0, 1, 1, 1);
                 } else if (i==2) {
-                    pieceLogic(0, 0, 0, 2, 2);
+                    pieceLogic(0, 0, 0, -2, 2);//CHANGED TO -2 from 2~~~
                 } else {
                     pieceLogic(1, 0, 1, 1, 3);
                 };
@@ -149,142 +116,8 @@ function myFunction(piece) {
             break;
     };
 };
-function piecestart(rowi,columnj, permutation) {
-    var i = 0;
-    var colOffset = 0;
-    var rowOffset = 0;
-    var findrow = 0;
-    var findcol = 0;  
-    var chooseBlock = [];
-//    for (i=0; i<blocks[0].length; i++) {
-//        console.log(blocks[0][i]);
-//    };
-    switch (permutation) {
-        case 0:
-            chooseBlock = blocks[0];
-            break;
-        case 1:
-            chooseBlock = blocks[1];
-            break;
-        case 2:
-            chooseBlock = blocks[2];
-            break;
-        case 3:
-            chooseBlock = blocks[3];
-            break;
-    };
-//    console.log(chooseBlock);
-   for (i=0; i<4; i++) {
-//       console.log(chooseBlock[i]);
-        colOffset = chooseBlock[i][0];
-        rowOffset = chooseBlock[i][1];
-        findrow = rowi - rowOffset;
-        findcol = columnj + colOffset;
-        $('.board').children('#'+findrow).children('#'+findcol).addClass(blockClass).addClass("1");
-   };
-//    console.log(rowi+","+columnj+","+permutation);
-    setTimeout(function() {clearrow(); }, 250);
-    //at the end of this function, run function to check if game is still being played!
-    //or maybe this should be played before the for loop, so that the game can be over and then you can visually see why it's over
-}
-function checkBottom(X, Y) {
-    //if next row is bottom, add 1
-    //else check if div in that position in next row has class "1", if so add 1
-    if (X > height-1) {
-        score += 1;
-    } else {
-            if ($('.board').children('#'+X).children('#' +Y).hasClass("1")) {
-            score +=1;
-        } else {
-            score -=1;
-        };
-    };
-    return score;
-}
-function checkRight(X, Y) {
-    //if next div is border, add 1
-    //else check if div in that position in next col has class "1", if so add 1
-    if (Y > width-1) {
-        score +=1;
-    } else {
-        if( $('.board').children('#'+X).children('#'+Y).hasClass("1")) {
-            score +=1;
-        }
-    }
-    return score;
-}
-function checkLeft(X, Y) {
-    //else check if div in that position in prev col has class "1", if so add 1
-//    console.log("checkLeft") 
-//    console.log(X+","+Y);
-    if (Y < 0) {
-        score +=1;
-    } else {
-        if( $('.board').children('#'+X).children('#'+Y).hasClass("1")) {
-            score +=1;
-        }
-    }
-    return score;
-}
-function findrows() {
-    var i;
-    var divs = [];
-//    console.log("running1");
-    for (i=0; i<height; i++) {
-        $('.board').children("#"+i).children().each(function() {
-            if ($(this).hasClass("1")) {
-//                console.log("runnig");
-                divs.push("true");
-            };
-        });
-        if (divs.length>0) {
-//            console.log("running2");
-            rows.push(i);
-            rows.push(i-1);
-        } else {
-            rows.push((height-1));
-        };
-    }
-    rows = $.unique(rows);
-    return rows;
-};
-function clearrow() {
-    var clearRows = [];
-    var i;
-    var divs = [];
-   for (i=0; i<height; i++) {
-        $('.board').children("#"+i).children().each(function() {
-            if ($(this).hasClass("1")) {
-                divs.push("true");
-            };
-            if (divs.length==width) {
-            clearRows.push(i);
-        };
-        });
-       divs = [];
-   };
-    clearRows = $.unique(clearRows);
-    i = 0;
-//    console.log(clearRows);
-//    console.log(clearRows.length);
-//    console.log(clearRows[0]);
-    if (clearRows.length > 0) {
-    for (i=0; i<clearRows.length; i++) {
-//        console.log("running");
-        var takeOut = $('.board').children("#"+clearRows[i]);
-        takeOut.remove();
-//        console.log("done");
-        $('.board').children("#"+clearRows[i]).remove($(this));
-    };
-    start = num;
-    num += clearRows.length;
-//    console.log(clearRows);
-//    console.log(clearRows.length);
-    logscore(clearRows.length);
-    createboard();
-    };
-};
-function pieceLogic(A, B, C, D, E, F, G) {//E for rotation, F-H for unique cases
+//DETERMINE SCORE OF ROTATED PIECES BASED ON HOW MANY EDGES ARE TOUCHING SOMETHING ELSE
+function pieceLogic(A, B, C, D, E, F, G) {//E for rotation, F-G for unique cases
     var i = 0;
     var j = 0;
     var colOffset = 0;
@@ -294,7 +127,7 @@ function pieceLogic(A, B, C, D, E, F, G) {//E for rotation, F-H for unique cases
     var checkpos = [];
     var indices = [];
     var colOffsets = [];
-    findrows();//find highest row# that does not contain any div's with classes
+    findrows();
     //get minimum colOffset
     for (i=0; i<4; i++) {
         colOffset = block[i][0];
@@ -309,7 +142,7 @@ function pieceLogic(A, B, C, D, E, F, G) {//E for rotation, F-H for unique cases
         if (Math.round($(this).attr('id'))+minColOff >= 0) {
             checkpos = [];
             score = 0;
-            for (i=0; i<4; i++) {//4 hard coded b/c every piece has 4!
+            for (i=0; i<4; i++) {//4 hard coded b/c every piece has 4 components
                 colOffset = block[i][0];
                 rowOffset = block[i][1];
                 if (!($('.board').children('#'+(rows[j]-rowOffset)).children('#'+(Math.round($(this).attr('id'))+colOffset)).hasClass("1"))) {
@@ -337,7 +170,7 @@ function pieceLogic(A, B, C, D, E, F, G) {//E for rotation, F-H for unique cases
                                 checkLeft((rows[j]-rowOffset),(Math.round($(this).attr('id')) + colOffset - 1));
                                 if (F == 1) {
                                     if (rowOffset == G) {
-                                        checkLeft((rows[j]-rowOffset),(Math.round($(this).attr('id')) + colOffset - 1));
+                                        checkRight((rows[j]-rowOffset), (Math.round($(this).attr('id')) + colOffset + 1));
                                     };
                                 };
                             };
@@ -379,6 +212,72 @@ function pieceLogic(A, B, C, D, E, F, G) {//E for rotation, F-H for unique cases
         };
     };
 }; 
+var rows = [];
+//FIND HIGHEST ROW# THAT DOES NOT CONTAIN DIVs WITH CLASSES
+function findrows() {
+    var i;
+    var divs = [];
+//    console.log("running1");
+    for (i=0; i<height; i++) {
+        $('.board').children("#"+i).children().each(function() {
+            if ($(this).hasClass("1")) {
+//                console.log("runnig");
+                divs.push("true");
+            };
+        });
+        if (divs.length>0) {
+//            console.log("running2");
+            rows.push(i);
+            rows.push(i-1);
+        } else {
+            rows.push((height-1));
+        };
+    }
+    rows = $.unique(rows);
+    return rows;
+};
+var score;
+function checkBottom(X, Y) {
+    //if next row is bottom, add 1
+    //else check if div in that position in next row has class "1", if so add 1
+    if (X > height-1) {
+        score += 1;
+    } else {
+            if ($('.board').children('#'+X).children('#' +Y).hasClass("1")) {
+            score +=1;
+        } else {
+            score -=1;
+        };
+    };
+    return score;
+}
+function checkRight(X, Y) {
+    //if next div is border, add 1
+    //else check if div in that position in next col has class "1", if so add 1
+    if (Y > width-1) {
+        score +=1;
+    } else {
+        if( $('.board').children('#'+X).children('#'+Y).hasClass("1")) {
+            score +=1;
+        }
+    }
+    return score;
+}
+function checkLeft(X, Y) {
+    //else check if div in that position in prev col has class "1", if so add 1
+//    console.log("checkLeft") 
+//    console.log(X+","+Y);
+    if (Y < 0) {
+        score +=1;
+    } else {
+        if( $('.board').children('#'+X).children('#'+Y).hasClass("1")) {
+            score +=1;
+        }
+    }
+    return score;
+}
+
+var solutions = [];
 function chooseSolution() {
     var indices = [];
     var solutions2 = [];
@@ -391,9 +290,9 @@ function chooseSolution() {
             solutions2.push(solutions[i]);
         };
     };
-//    for (i=0; i<solutions2.length; i++) {
-//        console.log(solutions2[i]);
-//    };
+    for (i=0; i<solutions2.length; i++) {
+        console.log(solutions2[i]);
+    };
     //if solutions2 are greater than 1, choose just 1 array
     if (solutions2.length > 1) {
         solutions2 = solutions2[Math.round(Math.random() * (solutions2.length - 1))];
@@ -401,8 +300,82 @@ function chooseSolution() {
     } else {
         piecestart(solutions2[0][0],solutions2[0][1], solutions2[0][3]);
     };
+    console.log(solutions2);
     solutions= [];
 };
+//PLACE THE SOLUTION PIECE ON THE BOARD
+function piecestart(rowi, columnj, permutation) {
+    var i = 0;
+    var colOffset = 0;
+    var rowOffset = 0;
+    var findrow = 0;
+    var findcol = 0;  
+    var chooseBlock = [];
+    switch (permutation) {
+        case 0:
+            chooseBlock = blocks[0];
+            break;
+        case 1:
+            chooseBlock = blocks[1];
+            break;
+        case 2:
+            chooseBlock = blocks[2];
+            break;
+        case 3:
+            chooseBlock = blocks[3];
+            break;
+    };
+   for (i=0; i<4; i++) {
+       colOffset = chooseBlock[i][0];
+       rowOffset = chooseBlock[i][1];
+       findrow = rowi - rowOffset;
+       findcol = columnj + colOffset;
+       $('.board').children('#'+findrow).children('#'+findcol).addClass(blockClass).addClass("1");
+   };
+    setTimeout(function() {clearrow(); }, 250);
+    //at the end of this function, run function to check if game is still being played!
+    //or maybe this should be played before the for loop, so that the game can be over and then you can visually see why it's over
+}
+//REMOVE ANY COMPLETE ROWS FROM THE BOARD
+function clearrow() {
+    var clearRows = [];
+    var i;
+    var divs = [];
+   for (i=0; i<height; i++) {
+        $('.board').children("#"+i).children().each(function() {
+            if ($(this).hasClass("1")) {
+                divs.push("true");
+            };
+            if (divs.length==width) {
+            clearRows.push(i);
+        };
+        });
+       divs = [];
+   };
+    clearRows = $.unique(clearRows);
+    i = 0;
+    if (clearRows.length > 0) {
+    for (i=0; i<clearRows.length; i++) {
+        var takeOut = $('.board').children("#"+clearRows[i]);
+        takeOut.remove();
+        $('.board').children("#"+clearRows[i]).remove($(this));
+    };
+    start = num;
+    num += clearRows.length;
+    logscore(clearRows.length);
+    createboard();
+    };
+};
+//ADD NUMBER OF CLEARED ROWS TO TOTAL USER SCORE
+function logscore(X) {
+    rowScore += X;
+    $(".notice3").remove();
+    if (rowScore > 0) {
+        $("#notes").append('<div class="notice3">Number of rows cleared: '+rowScore+'</div>');
+    };
+};
+
+
 //NEXT: run function that checks for complete rows and removes them (DONE)
 //NEXT: add function that changes all id's to numerical order (DONE)
 //NEXT: add jQuery that adds to row id=1 a thick red border on the top (DONE)
